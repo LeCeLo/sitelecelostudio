9d3959122b5413f0a10f3bc177339aea5594e1ef
LeCeLo <leandrocesarlobato@gmail.com>
2022-03-12 16:49:38 -0300

    first commit

---
+++ jscript/base.js
@@ -0,0 +1,11 @@
+
+
+ function RecolheMenu(){
+     document.querySelector("menuPrincipal");
+
+ }
+ function recolheMenu() {
+    let element = document.querySelector('.menuPrincipal-lista');
+    element.classList.toggle('menuPrincipal-lista-botao');
+
+  }
